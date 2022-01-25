f365194595d8ec14cf69b3de046eea0a47a86361
checheliuk <sasha.checheliuk@gmail.com>
2022-01-25 11:19:26 +0200

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,6 +18,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -30,6 +31,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
             
